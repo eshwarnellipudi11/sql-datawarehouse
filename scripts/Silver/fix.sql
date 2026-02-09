5be441b128b7b0e37a50ec3229eee9eb45fdd148
Eshwar Nellipudi <eshwarnellipudi@yahoo.com>
2026-02-09 19:26:41 +0530

    Update fix.sql for crm_prd_info
    Insert data into silver.crm_prd_info with transformations.

--- scripts/Silver/fix.sql
+++ scripts/Silver/fix.sql
@@ -26,3 +26,35 @@ from
 ROW_NUMBER() OVER(partition by cst_id Order by cst_create_date Desc) as cust_rank_by_date
 from bronze.crm_cust_info) As t
 Where cust_rank_by_date = 1 AND Cst_id IS NOT NULL;
+
+
+
+-- Inserting the data in silver.crm_prd_info --
+
+
+INSERT INTO silver.crm_prd_info(
+	prd_id,
+	cat_id,
+	prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+)
+Select 
+prd_id,
+replace(substring(prd_key,1,5),'-', '_') AS cat_id,
+substring(prd_key,7, len(prd_key)) As prd_key,
+prd_nm,
+ISNULL (prd_cost,0) As prd_cost,
+  CASE
+	WHEN UPPER(trim(prd_line)) = 'M' Then 'Mountain'
+	WHEN UPPER(trim(prd_line)) = 'S' Then 'Other Size'
+	WHEN UPPER(trim(prd_line)) = 'T' Then 'Touring'
+	WHEN UPPER(trim(prd_line)) = 'R' Then 'Road'
+else 'n/a'
+End As prd_line,
+CAST(prd_start_dt as date) As prd_start_dt,
+CAST(lead(prd_start_dt) Over(partition by prd_key Order By prd_start_dt) - 1 as date) AS prd_end_dt
+from bronze.crm_prd_info;
