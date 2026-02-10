d0647a817686b8bd9f1001b1fed3df0beb241066
Eshwar Nellipudi <eshwarnellipudi@yahoo.com>
2026-02-10 12:04:27 +0530

    silver.erp_cust_az12

--- scripts/Silver/fix.sql
+++ scripts/Silver/fix.sql
@@ -58,3 +58,68 @@ CAST(prd_start_dt as date) As prd_start_dt,
 CAST(lead(prd_start_dt) Over(partition by prd_key Order By prd_start_dt) - 1 as date) AS prd_end_dt
 from bronze.crm_prd_info;
 
+-- Inserting the data in silver.crm_sales_details --
+
+INSERT INTO silver.crm_sales_details (
+			sls_ord_num,
+			sls_prd_key,
+			sls_cust_id,
+			sls_order_dt,
+			sls_ship_dt,
+			sls_due_dt,
+			sls_sales,
+			sls_quantity,
+			sls_price
+		)
+		SELECT 
+			sls_ord_num,
+			sls_prd_key,
+			sls_cust_id,
+			CASE 
+				WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
+				ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+			END AS sls_order_dt,
+			CASE 
+				WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
+				ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+			END AS sls_ship_dt,
+			CASE 
+				WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
+				ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+			END AS sls_due_dt,
+			CASE 
+				WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price) 
+					THEN sls_quantity * ABS(sls_price)
+				ELSE sls_sales
+			END AS sls_sales,
+			sls_quantity,
+			CASE 
+				WHEN sls_price IS NULL OR sls_price <= 0 
+					THEN sls_sales / NULLIF(sls_quantity, 0)
+				ELSE sls_price 
+			END AS sls_price
+		FROM bronze.crm_sales_details;
+
+
+-- Inserting the data in silver.erp_cust_az12 --
+
+Insert Into silver.erp_cust_az12(
+	cid,
+	bdate,
+	gen
+)
+Select
+	CASE
+	WHEN cid Like 'NAS%' THEN SUBSTRING(cid, 4,Len(cid)) 
+	Else cid
+	End As cid,
+	CASE
+	WHEN bdate > getdate() then NULL
+	else bdate
+	End As bdate,
+	CASE
+	When UPPER(TRIM(gen)) IN ('M', 'Male') THEN 'Male'
+	When UPPER(TRIM(gen)) IN ('F', 'Female') THEN 'Female'
+	Else 'n/a'
+	End As gen
+from bronze.erp_cust_az12;
